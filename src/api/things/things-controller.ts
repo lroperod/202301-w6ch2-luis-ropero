b7a31d3ea6900800af9a659d4535e00829fab40e
luis <luisrym@gmail.com>
2023-02-24 00:30:37 +0100

    refactor functions crud

--- src/api/things/things-controller.ts
+++ src/api/things/things-controller.ts
@@ -1,35 +1,75 @@
+import { ThingModel } from './thing-schema.js';
 import { RequestHandler } from 'express';
 import crypto from 'node:crypto';
-import { createThings, findAll, findById, Things } from './things-model.js';
+import { Things } from './things-model.js';
 
-export const getThingsController: RequestHandler = (_req, res) => {
-  res.json(findAll());
-};
-
-export const createThingController: RequestHandler = (req, res) => {
-  const id = crypto.randomUUID();
-  const things: Things = {
-    id,
-    ...req.body,
-  };
-  createThings(things);
-  res.status(201).json(things);
-};
-
-export const getThingByIdController: RequestHandler = (req, res) => {
-  const { id } = req.params;
-  const things = findById(id);
-  if (things === undefined) {
-    res.sendStatus(404);
-  } else {
-    res.json(things);
+export const getThingsController: RequestHandler = async (_req, res) => {
+  try {
+    const foundThings = await ThingModel.find({});
+    res.json(foundThings);
+  } catch (error) {
+    res.status(500).json(error);
   }
 };
 
-export const updateThingByIdController: RequestHandler = (req, res) => {
-  res.sendStatus(405);
+export const createThingController: RequestHandler = async (req, res) => {
+  const id = crypto.randomUUID();
+  const thing: Things = {
+    id,
+    ...req.body,
+  };
+  try {
+    await ThingModel.create(thing);
+    res.status(201).json(thing);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
-export const deleteThingByIdController: RequestHandler = (req, res) => {
-  res.sendStatus(405);
+export const getThingByIdController: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const thing = await ThingModel.findById(id);
+    if (thing === null) {
+      res.sendStatus(404);
+    } else {
+      res.json(thing);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const updateThingByIdController: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const dbRes = await ThingModel.updateOne({ _id: id }, { ...req.body });
+    if (dbRes.matchedCount === 0) {
+      res.sendStatus(404);
+    }
+
+    if (dbRes.modifiedCount === 1) {
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(500);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const deleteThingByIdController: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const dbRes = await ThingModel.deleteOne({ _id: id });
+    if (dbRes.deletedCount === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
