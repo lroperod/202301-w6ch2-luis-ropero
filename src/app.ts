dc373125e990d3112b87f252f17c8679964709fa
luis <luisrym@gmail.com>
2023-02-23 06:17:10 +0100

    delete code smells and change response when server is up

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Server is up!!');
 });
 app.use(express.json());
 app.use('/api/v1', apiRouter);
-
-// App.post('/', (req, res) => {
-//   res.json('Hello World fucking Indio!');
-// });
 
 app.listen(port, () => `Example app listening on port ${port}`);
