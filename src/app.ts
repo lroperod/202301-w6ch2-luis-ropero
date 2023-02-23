c613d2e830139ba12c381c82e852295f4f408386
luis <luisrym@gmail.com>
2023-02-23 06:05:39 +0100

    add new route to raise server

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import apiRouter from './api/api-router.js';
 
 const app = express();
 const port = 3000;
@@ -6,9 +7,11 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.use(express.json());
+app.use('/api/v1', apiRouter);
 
-app.post('/', (req, res) => {
-  res.json('Hello World fucking Indio!');
-});
+// App.post('/', (req, res) => {
+//   res.json('Hello World fucking Indio!');
+// });
 
 app.listen(port, () => `Example app listening on port ${port}`);
